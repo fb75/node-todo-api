758ef0c31aded82af382dbc0eba1e85b09536843
Francesco Brachini <gunner19751@gmail.com>
2017-08-31 16:26:39 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -33,6 +34,27 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/12345678
+// :id creates a variabile inside req object
+app.get('/todos/:id', (req, res) => {
+  // using somthing off the req
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    console.log('Id not valid');
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
