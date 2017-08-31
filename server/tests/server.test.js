b8033838ce218a2b06ed1782227d6fed7ae031e5
Francesco Brachini <gunner19751@gmail.com>
2017-08-31 10:50:19 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-// to run the test the todo must be = 0 in the db
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
+// Populating the db with objects
 beforeEach((done) => {
-  Todo.remove({}).then(() =>  done());
+  Todo.remove({}).then(() =>  {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -17,14 +25,14 @@ describe('POST /todos', () => {
     request(app)
       .post('/todos')
       .send({text})
-    .expect(200)
-    .expect((res) => {
+      .expect(200)
+      .expect((res) => {
       expect(res.body.text).toBe(text);
     }).end((err,res) => {
       if (err) {
         return done(err);
       } // using .find method (not the mongodb method) to see if todos exist on db
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -42,10 +50,21 @@ describe('POST /todos', () => {
           return done(err)
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
 
+  // single test case
+  describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+      request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.length).toBe(2);
+        }).end(done);
+    });
   });
 });
