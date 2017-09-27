4c09cb349c0a550b0fc65cd305237efadacd42fd
Francesco Brachini <gunner19751@gmail.com>
2017-09-27 17:02:54 +0200

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -82,7 +82,6 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 // Route for updating Todo items
-// installing lodash first: npm i --save lodash
 app.patch('/todos/:id', (req, res) => {
   // get the id
   var id = req.params.id;
@@ -112,6 +111,22 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   });  
 });
+
+// POST /users
+app.post('/users', (req, res) => {
+  
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  
+  user.save().then(() => {   
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);  
