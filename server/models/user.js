246c85cff527c98614fb83f9d7a3627b5ad18c64
Francesco Brachini <gunner19751@gmail.com>
2017-09-28 21:19:26 +0200

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 // defining Schema for the User model
 var UserSchema = new mongoose.Schema({	
@@ -75,6 +76,25 @@ UserSchema.statics.findByToken = function(token) {
 		'tokens.access': 'auth'
 	});
 };
+
+// using mongoose middleware to fire event before save event
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	// checking if password has been already hashed
+	if (user.isModified('password')) {
+			var user = this;
+			var password = user.password;
+			bcrypt.genSalt(10, (err, salt) => {
+		 		bcrypt.hash(password, salt, (err, hash) => {
+		 		user.password = hash;
+		 		next();
+		 	});
+	 	});
+ 	}	else {
+ 		next(); 
+ }
+});
 
 var User = mongoose.model('User', UserSchema);
 
