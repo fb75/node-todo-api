1a7874fc3613ecb8939c4f9f5b887fc955b183ad
Francesco Brachini <gunner19751@gmail.com>
2017-10-04 17:35:56 +0200

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -321,3 +321,28 @@ describe('POST /users/login', () => {
 
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)   
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+
+    });
+    // DELETE /users/me/token
+    // Set x-auth token = token
+    // 200
+    // Find user, verify that tokens array has length of zero
+  });
+});
